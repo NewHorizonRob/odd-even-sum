15421806b504d5cf3cdef4346a841db71005f361
NewHorizonRob <53019786+NewHorizonRob@users.noreply.github.com>
2019-07-23 16:18:33 -0400

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch3_16
+{
+    class Class1
+    {
+        public void Bigger()
+
+        {
+         //   int z;
+            int x;
+            Console.Write("Enter a number: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+           if (x % 2 == 0)
+            {
+                Console.WriteLine("The number is even ");
+            }
+
+            if (x % 2 != 0)
+            {
+                Console.WriteLine("The number is odd ");
+            }
+
+            int y;
+            Console.Write("Enter a number: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            int z = y + x;
+            
+            Console.WriteLine($"The answer is {z}");
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch3_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c= new Class1();
+            c.Bigger();
+            Console.WriteLine("PAK");
+            Console.ReadKey();
+
+        }
+    }
+}
