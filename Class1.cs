45f1d11d16160c0384b0ab2ae5f3251ac5dbf37d
NewHorizonRob <53019786+NewHorizonRob@users.noreply.github.com>
2019-07-24 19:09:59 -0400

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -8,31 +8,31 @@ namespace Ch3_16
 {
     class Class1
     {
-        public void Bigger()
+        public void Bigger() #method to see if the number is even odd and sums up two numbers
 
         {
          //   int z;
-            int x;
-            Console.Write("Enter a number: ");
-            x = Convert.ToInt32(Console.ReadLine());
+            int x; #initializes the x int
+            Console.Write("Enter a number: "); #prompts the user
+            x = Convert.ToInt32(Console.ReadLine()); #converts the x variable
 
-           if (x % 2 == 0)
+           if (x % 2 == 0) #if the x % 2 is equal to 0 then it is even
             {
                 Console.WriteLine("The number is even ");
             }
 
-            if (x % 2 != 0)
+            if (x % 2 != 0) if x % 0 doesnt equal 0 then the it is odd
             {
                 Console.WriteLine("The number is odd ");
             }
 
-            int y;
-            Console.Write("Enter a number: ");
-            y = Convert.ToInt32(Console.ReadLine());
+            int y; # initializes the y int 
+            Console.Write("Enter a number: "); #prompts the user 
+            y = Convert.ToInt32(Console.ReadLine()); #converts the y variable 
 
-            int z = y + x;
+            int z = y + x; #sums up the x and y variable 
             
-            Console.WriteLine($"The answer is {z}");
+            Console.WriteLine($"The answer is {z}"); #writes out the message and the sum to the console
         }
     }
 }
